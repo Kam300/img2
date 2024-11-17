0983a62c4b7665bb4161ce26067a69dcb2a441a4
Kam300 <kamilgaripov205@gmail.com>
2024-11-17 23:09:42 +0500

    first commit

---
+++ ImageRotator.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+
+namespace ImageViewer
+{
+    public partial class ImageRotator : UserControl
+    {
+        private double angle = 0; // угл поворота 
+
+        public ImageRotator()
+        {
+            InitializeComponent();
+        }
+
+        public void SetImageSource(string imageUrl)
+        {
+            image.Source = new BitmapImage(new Uri(imageUrl, UriKind.Absolute)); //источн изобр
+        }
+
+        private void RotateLeft_Click(object sender, RoutedEventArgs e)
+        {
+            RotateImage(-90);
+        }
+
+        private void RotateRight_Click(object sender, RoutedEventArgs e)
+        {
+            RotateImage(90);
+        }
+
+        private void RotateImage(double rotationAngle)
+        {
+            var rotateTransform = image.RenderTransform as RotateTransform; // Получ тек Rotat
+            if (rotateTransform == null)
+            {
+                rotateTransform = new RotateTransform(0);
+                image.RenderTransform = rotateTransform;
+                image.RenderTransformOrigin = new Point(0.5, 0.5); //центр вра
+            }
+
+            angle += rotationAngle;
+            var animation = new DoubleAnimation
+            {
+                From = rotateTransform.Angle,
+                To = angle,
+                Duration = TimeSpan.FromMilliseconds(300),
+                EasingFunction = new QuarticEase() 
+            };
+
+            rotateTransform.BeginAnimation(RotateTransform.AngleProperty, animation);
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace ImageViewer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadImages();
+        }
+
+        private void LoadImages()
+        {
+            imageRotator1.SetImageSource("https://avatars.mds.yandex.net/i?id=1445c569fce2f3115d5628f50911f075_l-12522553-images-thumbs&n=13");
+            imageRotator2.SetImageSource("https://avatars.mds.yandex.net/i?id=6a22114b43f8b891f90e2fdbf2ebc760_l-5236641-images-thumbs&n=13");
+            imageRotator3.SetImageSource("https://avatars.mds.yandex.net/i?id=44bf462830e2bb273caa2ef21252e8f2_l-4268172-images-thumbs&n=13");
+            imageRotator4.SetImageSource("https://u-stena.ru/upload/iblock/806/806cbf9648de83c02c38388d8a19800b.jpg");
+            imageRotator5.SetImageSource("https://cdnstatic.rg.ru/uploads/images/197/50/00/matterhorn-918442_1920.jpg");
+            imageRotator6.SetImageSource("https://komionline.ru/uploads/2018/09/CF000816.jpg");
+
+        }
+    }
+}
